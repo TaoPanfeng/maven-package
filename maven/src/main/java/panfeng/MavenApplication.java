9ac37eed5f8366f54204594113d6ed64d56e8f4c
TaoPanfeng <1801957499@qq.com>
2019-09-19 16:17:23 +0800

    commit

---
+++ maven/src/main/java/panfeng/MavenApplication.java
@@ -0,0 +1,13 @@
+package panfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MavenApplication.class);
+    }
+}
